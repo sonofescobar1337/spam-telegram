c887f6c51555823fdea08937ee7db5456b288e0b
sonofescobar1337 <argrdotid@gmail.com>
2024-02-23 16:48:09 +0700

    update alur script

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import random
 import string
 from concurrent.futures import ThreadPoolExecutor
 
-
-def make_http_request(request_number):
-
-
+def make_http_request(request_number, message, chat_id, bot_token):
     with requests.Session() as session:
         try:
             response = session.post(
-                'https://api.telegram.org/bot6405005257:AAEByrxD0CoeSJGgWv4butnGwxATsRKtlLY/sendMessage',
+                f'https://api.telegram.org/{bot_token}/sendMessage',
                 params={
                     'parse_mode': 'markdown',
-                    'chat_id': '6656200257',
-                    'text': f'MENDING TOBAT BANG AWOWKOWKOWKOWKWOKWOK BY SONOFESCOBAR1337'
-                },
-                timeout=10
+                    'chat_id': chat_id,
+                    'text': message
+                }
             )
 
             response_json = response.json()
@@ -27,12 +23,20 @@ def make_http_request(request_number):
         except Exception as e:
             print("request timeout bang")
 
+# Membaca pesan dan chat id target dari file
+with open("pesan.txt", "r") as pesan_file:
+    message = pesan_file.read()
+
+with open("target.txt", "r") as target_file:
+    chat_id = target_file.read()
+
+with open("token.txt", "r") as token_file:
+    bot_token = token_file.read().strip()
 
 num_threads = int(input("Masukkan jumlah thread yang ingin digunakan: "))
-
 
 with ThreadPoolExecutor(max_workers=num_threads) as executor:
     request_count = 0
     while True:
         request_count += 1
-        executor.submit(make_http_request, request_count)
+        executor.submit(make_http_request, request_count, message, chat_id, bot_token)
